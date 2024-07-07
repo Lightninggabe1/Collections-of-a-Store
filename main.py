59eb821ac2979a69fbea9468d5f06345b85b35e4
Lightninggabe1 <lightninggabe191@gmail.com>
2024-07-07 15:03:44 +0800

    Create main.py:
    new mod ule :
    rec ap     
     py thon 
    cont aine rs

---
+++ main.py
@@ -0,0 +1,6 @@
+#new module: "Recap: python Containers"
+# Write your code below!
+company_name = "®"
+company_location = ('0.267127227218127','1234.87719214812984368318309131')
+company_products = ['aPads','Datum Reading Platform','Prey MB-225 Multiprocessor Computer','Menclature Janus Console','5V Adapter']
+company_data = {'name':company_name, 'location':company_location,'products':company_products}
