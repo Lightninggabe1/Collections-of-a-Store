cde3753f2fe5735f237e8e008cc3a11698785f6c
Lightninggabe1 <lightninggabe191@gmail.com>
2024-07-08 15:26:30 +0800

    Create helper_functions.py (For Deque module)
    Needs `csv.py`

---
+++ helper_functions.py
@@ -0,0 +1,9 @@
+import csv
+
+def process_csv_supplies():
+    data = []
+    with open('supplies_data.csv', 'r') as csvfile:
+        r = csv.reader(csvfile)
+        for row in r:
+            data.append(row)
+    return data
