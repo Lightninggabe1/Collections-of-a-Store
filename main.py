f7cd0bf0fb8febf2fc4f130a73c95e80fd42b4b4
Lightninggabe1 <lightninggabe191@gmail.com>
2024-07-12 18:42:11 +0800

    Update main.py: Ordered Dictionaries
    This is an update of main.py

--- main.py
+++ main.py
@@ -67,3 +67,41 @@ validated_locations = defaultdict(lambda: 'TODO: Add to website')
 validated_locations.update(site_locations)
 for product in updated_products:
   site_locations[product] = validated_locations[product]
+
+#new_module: 'OrderedDict' 
+from collections import OrderedDict
+
+# The first 15 orders are provided
+order_data = [['Order: 1', 'purchased'],
+              ['Order: 2', 'purchased'],
+              ['Order: 3', 'purchased'],
+              ['Order: 4', 'returned'],
+              ['Order: 5', 'purchased'],
+              ['Order: 6', 'canceled'],
+              ['Order: 7', 'returned'],
+              ['Order: 8', 'purchased'],
+              ['Order: 9', 'returned'],
+              ['Order: 10', 'canceled'],
+              ['Order: 11', 'purchased'],
+              ['Order: 12', 'returned'],
+              ['Order: 13', 'purchased'],
+              ['Order: 14', 'canceled'],
+              ['Order: 15', 'purchased']]
+
+# Write your code below!
+orders = OrderedDict(order_data)
+to_move = []
+to_remove = []
+for order, value in orders.items():
+  if value == 'returned':
+    to_move.append(order)
+  elif value == 'canceled':
+    to_remove.append(order)
+
+for removal in to_remove:
+  orders.pop(removal)
+
+for returnee in to_move:
+  orders.move_to_end(returnee)
+
+print(orders)
